a70f5f9f66b000f39f8748feb945f13f54fafd5a
kyohei_i <kyoheishogun.1997@i.softbank.jp>
2021-11-12 20:50:15 +0900

    プロジェクト作成

---
+++ MailLinkAuth-Sample/MailLinkViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MailLinkViewController.swift
+//  MailLinkAuth-Sample
+//
+//  Created by 今村京平 on 2021/11/12.
+//
+
+import UIKit
+
+class MailLinkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
